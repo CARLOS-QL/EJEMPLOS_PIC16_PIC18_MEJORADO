de640ae41fe04536b9aec27a0f9449a31e3314a7
Carlos Quispe <carlos359_7@hotmail.com>
2022-08-12 00:48:50 -0500

    Add files via upload

---
+++ rgb_pic16f18426.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=rgb_pic16f18426.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/rgb_pic16f18426.X.production.hex
